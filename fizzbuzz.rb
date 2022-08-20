cf3f0d5599137f09488e0da50e0f3981aefadd56
cedricn8 <cedcn-9mail@yahoo.com>
2022-08-19 19:34:26 -0700

    commit tree f595751a097aaf590b18dbff29c44dd14eb9a96f

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(value)
+    if value == 3 
+        text = "Fizz"
+    elsif value == 5
+        text = "Buzz"
+    elsif value == 15 
+        text = "FizzBuzz"
+    end
+end
